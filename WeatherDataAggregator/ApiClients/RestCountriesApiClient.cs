34cdc07b3161fcbd380b66f0b423c258b8cdcc57
Mikolaj Tworus <mikolaj7933333@gmail.com>
2025-04-17 14:22:50 +0200

    add RestCountries api client

---
+++ WeatherDataAggregator/ApiClients/RestCountriesApiClient.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.ObjectModel;
+
+namespace WeatherDataAggregator.ApiClients;
+
+public class RestCountriesApiClient
+{
+    private const string _urlBase = @"https://restcountries.com/v3.1/region/";
+    private readonly ReadOnlyDictionary<Continent, string> _continentSelector =
+        new ReadOnlyDictionary<Continent, string>(new Dictionary<Continent, string>()
+        {
+            [Continent.Europe] = "europe",
+            [Continent.Asia] = "asia",
+            [Continent.Africa] = "africa",
+            [Continent.NorthAmerica] = "north america",
+            [Continent.SouthAmerica] = "south america",
+            [Continent.Australia] = "australia"
+        });
+
+    public async Task<string> FetchData(Continent continent)
+    {
+        string fullUrl = $"{_urlBase}{_continentSelector[continent]}";
+        using var httpClient = new HttpClient();
+        var response = await httpClient.GetAsync(fullUrl);
+        response.EnsureSuccessStatusCode();
+        return await response.Content.ReadAsStringAsync();
+    }
+}

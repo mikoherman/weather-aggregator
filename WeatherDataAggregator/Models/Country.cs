c3b5be8d3f7f56ca483d824085da421f88aee2cc
Mikolaj Tworus <mikolaj7933333@gmail.com>
2025-04-17 13:39:53 +0200

    add first model class

---
+++ WeatherDataAggregator/Models/Country.cs
@@ -0,0 +1,11 @@
+﻿namespace WeatherDataAggregator.Models;
+
+public class Country
+{
+    public string Name { get; }
+    public double Latitude { get; }
+    public double Longitude { get; }
+
+    public override string ToString() =>
+        $"Country: {Name}, Located at: ({Latitude}, {Longitude})";
+}

3bde12f8fb35217daca22ae519d73f12a2acd92b
Mikolaj Tworus <mikolaj7933333@gmail.com>
2025-04-17 21:19:12 +0200

    add EnumExtensions for easier retrieval of all enum values

---
+++ WeatherDataAggregator/Utilities/EnumExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace WeatherDataAggregator.Utilities;
+
+public static class EnumExtensions
+{
+    /// <summary>
+    /// Returns an <see cref="IEnumerable{T}"/> containing all possible values of the specified enum type.
+    /// </summary>
+    /// <typeparam name="T">The enum type.</typeparam>
+    /// <returns>
+    /// An <see cref="IEnumerable{T}"/> of all values defined in the <typeparamref name="T"/> enum.
+    /// </returns>
+    public static IEnumerable<T> GetAllValues<T>() where T : Enum =>
+        Enum.GetValues(typeof(T)).Cast<T>();
+}

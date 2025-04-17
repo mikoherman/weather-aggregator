bc2c8bac6eedf141b02405025b58ee3ce6741795
Mikolaj Tworus <mikolaj7933333@gmail.com>
2025-04-17 21:17:49 +0200

    add Continent enum extension for easy display of all continents

---
+++ WeatherDataAggregator/Utilities/ContinentEnumExtensions.cs
@@ -0,0 +1,18 @@
+﻿using WeatherDataAggregator.Models;
+
+namespace WeatherDataAggregator.Utilities;
+
+public static class ContinentEnumExtensions
+{
+    /// <summary>
+    /// Returns a formatted string listing all values of the <see cref="Continent"/> enum,
+    /// with their corresponding integer values.
+    /// </summary>
+    /// <returns>
+    /// A string where each line contains the integer value and name of a <see cref="Continent"/> enum member.
+    /// </returns>
+    public static string GetFormattedList() =>
+        string.Join(Environment.NewLine, EnumExtensions
+            .GetAllValues<Continent>()
+            .Select(continent => $"{(int)continent}. {continent}"));
+}

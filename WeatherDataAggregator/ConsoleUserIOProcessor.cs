ff8c3f790bacf8ff49365bd2b94d7d70cb284a23
Mikolaj Tworus <mikolaj7933333@gmail.com>
2025-04-17 21:59:15 +0200

    add ConsoleUserIOProcessor for console I/O Processing

---
+++ WeatherDataAggregator/ConsoleUserIOProcessor.cs
@@ -0,0 +1,38 @@
+﻿using WeatherDataAggregator.Models;
+using WeatherDataAggregator.Utilities;
+
+namespace WeatherDataAggregator;
+
+public class ConsoleUserIOProcessor
+{
+    private readonly IConsoleUserInteractor _userInteractor;
+
+    public ConsoleUserIOProcessor(IConsoleUserInteractor userInteractor)
+    {
+        _userInteractor = userInteractor;
+    }
+
+    private void DisplayAllContinents()
+    {
+        var allContinentsToDisplay = 
+            ContinentEnumExtensions.GetFormattedList();
+        _userInteractor.DisplayMessage(allContinentsToDisplay);
+    }
+
+    public Continent PromptUserForContinent()
+    {
+        DisplayAllContinents();
+        string? userInput;
+        int continentEnumId;
+        do
+        {
+            _userInteractor.DisplayMessage("Please pick Continent Number");
+            userInput = _userInteractor.Prompt();
+        } while (
+        string.IsNullOrEmpty(userInput) ||
+        !int.TryParse(userInput, out continentEnumId) ||
+        !Enum.IsDefined(typeof(Continent), continentEnumId));
+        return (Continent) continentEnumId;
+    }
+    
+}

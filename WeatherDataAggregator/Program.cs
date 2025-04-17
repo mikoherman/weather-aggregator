c6073317086a53e65a924c867c26c27e4f523913
Mikolaj Tworus <mikolaj7933333@gmail.com>
2025-04-17 20:14:39 +0200

    temp modification of Program.cs

--- WeatherDataAggregator/Program.cs
+++ WeatherDataAggregator/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace WeatherDataAggregator
+﻿using WeatherDataAggregator.DataAccess;
+using WeatherDataAggregator.DataAccess.ApiClients;
+using WeatherDataAggregator.Models;
+
+namespace WeatherDataAggregator;
+
+public class Program
 {
-    internal class Program
+    static async Task Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        var apiFetcher = new RestCountriesApiClient();
+        var countryDataProvider = new CountryDataProvider(apiFetcher);
+        int counter = 1;
+        var countryCollection = (await countryDataProvider
+            .GetCountriesByContinentAsync(Continent.Europe))
+            .Select(country => $"{counter++}. {country}\n");
+        Console.WriteLine(string.Join("", countryCollection));
+
+        Console.WriteLine("Program is finished.");
+        Console.ReadKey();
     }
 }

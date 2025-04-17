448832b094f1a0376eec9b23336fdb9ec9e035ed
Mikolaj Tworus <mikolaj7933333@gmail.com>
2025-04-17 20:11:29 +0200

    change Country class properties to init-only

--- WeatherDataAggregator/Models/Country.cs
+++ WeatherDataAggregator/Models/Country.cs
@@ -2,9 +2,9 @@
 
 public class Country
 {
-    public string Name { get; }
-    public double Latitude { get; }
-    public double Longitude { get; }
+    public string Name { get; init; }
+    public double Latitude { get; init; }
+    public double Longitude { get; init; }
 
     public override string ToString() =>
         $"Country: {Name}, Located at: ({Latitude}, {Longitude})";

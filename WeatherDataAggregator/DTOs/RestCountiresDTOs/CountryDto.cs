438df93d544001896de818f70ce37aa318d4d407
Mikolaj Tworus <mikolaj7933333@gmail.com>
2025-04-17 13:38:27 +0200

    add first DTOs for RestCountries Api

---
+++ WeatherDataAggregator/DTOs/RestCountiresDTOs/CountryDto.cs
@@ -0,0 +1,7 @@
+﻿using System.Text.Json.Serialization;
+namespace WeatherDataAggregator.DTOs.RestCountiresDTOs;
+
+public record CountryDto(
+        [property: JsonPropertyName("name")] CountryNameDto Name,
+        [property: JsonPropertyName("latlng")] IReadOnlyList<double> LatLng
+);
---
+++ WeatherDataAggregator/DTOs/RestCountiresDTOs/CountryNameDto.cs
@@ -0,0 +1,6 @@
+﻿using System.Text.Json.Serialization;
+namespace WeatherDataAggregator.DTOs.RestCountiresDTOs;
+
+public record CountryNameDto(
+        [property: JsonPropertyName("common")] string Common
+);

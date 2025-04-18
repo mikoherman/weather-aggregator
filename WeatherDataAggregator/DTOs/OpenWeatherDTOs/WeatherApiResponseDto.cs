6510423bad12318b49a2d7508e2859f5de8201f8
Mikolaj Tworus <mikolaj7933333@gmail.com>
2025-04-18 13:26:01 +0200

    add DTOs for OpenWeatherApi

---
+++ WeatherDataAggregator/DTOs/OpenWeatherDTOs/WeatherApiResponseDto.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WeatherDataAggregator.DTOs.OpenWeatherDTOs;
+
+public record WeatherApiResponseDto(
+    [property: JsonPropertyName("weather")] IReadOnlyList<WeatherConditionDto> WeatherConditionDto,
+    [property: JsonPropertyName("main")] WeatherMainDto WeatherMainDto,
+    [property: JsonPropertyName("wind")] WindDto Wind
+);
---
+++ WeatherDataAggregator/DTOs/OpenWeatherDTOs/WeatherMainDto.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WeatherDataAggregator.DTOs.OpenWeatherDTOs;
+
+public record WeatherMainDto(
+        [property: JsonPropertyName("temp")] double Temp,
+        [property: JsonPropertyName("feels_like")] double FeelsLike,
+        [property: JsonPropertyName("pressure")] int Pressure,
+        [property: JsonPropertyName("humidity")] int Humidity
+);

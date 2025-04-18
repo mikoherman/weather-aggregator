0aed6d000f78f33ab3dc80727f3d827a0aba4454
Mikolaj Tworus <mikolaj7933333@gmail.com>
2025-04-18 20:06:11 +0200

    add interface for TablePrinter class to comply with dependency inversion principle

---
+++ WeatherDataAggregator/UserInteraction/TablePrinter/IConsoleWeatherDataTablePrinter.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.ObjectModel;
+using WeatherDataAggregator.Models;
+
+namespace WeatherDataAggregator.UserInteraction.TablePrinter;
+
+public interface IConsoleWeatherDataTablePrinter
+{
+    void PrintTable(ReadOnlyDictionary<Country, WeatherData> weathersInCountries);
+}

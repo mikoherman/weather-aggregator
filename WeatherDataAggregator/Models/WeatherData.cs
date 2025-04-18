70a69e64ae36c597be5f332acf82fff88a26ec02
Mikolaj Tworus <mikolaj7933333@gmail.com>
2025-04-18 17:00:22 +0200

    add WeatherData to represent weather data retrieved from the api

---
+++ WeatherDataAggregator/Models/WeatherData.cs
@@ -0,0 +1,18 @@
+﻿namespace WeatherDataAggregator.Models;
+
+public class WeatherData
+{
+    public string? WeatherDescription { get; init; }
+    public double Temperature { get; init; }
+    public double FeelsLikeTemperature { get; init; }
+    public double Humidity { get; init; }
+    public double WindSpeed { get; init; }
+    public double Pressure { get; init; }
+
+    public override string ToString() => $"Weather: {WeatherDescription}, " +
+        $"Temperature: {Temperature}°C, " +
+        $"Feels Like: {FeelsLikeTemperature}°C, " +
+        $"Humidity: {Humidity}%, " +
+        $"Wind Speed: {WindSpeed} m/s, " +
+        $"Pressure: {Pressure} hPa";
+}

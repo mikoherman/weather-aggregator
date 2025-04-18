9ba9ec5245627e241180a97249e47d04eb0d552b
Mikolaj Tworus <mikolaj7933333@gmail.com>
2025-04-18 20:17:29 +0200

    add interface for ConsoleUserIOProcessor class

---
+++ WeatherDataAggregator/UserInteraction/IConsoleUserIOProcessor.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.ObjectModel;
+using WeatherDataAggregator.Models;
+
+namespace WeatherDataAggregator.UserInteraction;
+
+public interface IConsoleUserIOProcessor
+{
+    void DisplayWeatherDataForCountries(ReadOnlyDictionary<Country, WeatherData> weathersInCountries);
+    Continent PromptUserForContinent();
+}

2e082d1129594d2c688428d43e7116a345680001
Mikolaj Tworus <mikolaj7933333@gmail.com>
2025-04-18 16:59:36 +0200

    add interface to abstract OpenWeatherApiClient class

---
+++ WeatherDataAggregator/DataAccess/ApiClients/IWeatherApiClient.cs
@@ -0,0 +1,9 @@
+﻿using WeatherDataAggregator.Models;
+
+namespace WeatherDataAggregator.DataAccess.ApiClients;
+
+public interface IWeatherApiClient
+{
+    void Dispose();
+    Task<string> GetWeatherData(Country country, string apiKey);
+}

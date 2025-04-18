5633f23000c38cef0aaa02158f38188768d8eb5f
Mikolaj Tworus <mikolaj7933333@gmail.com>
2025-04-18 20:16:24 +0200

    add interfaces for data provider classes

---
+++ WeatherDataAggregator/DataAccess/IWeatherDataProvider.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.ObjectModel;
+using WeatherDataAggregator.Models;
+
+namespace WeatherDataAggregator.DataAccess;
+
+public interface IWeatherDataProvider
+{
+    Task<ReadOnlyDictionary<Country, WeatherData>> GetWeatherDataForCountriesAsync(IEnumerable<Country> countries, string? apiKey);
+}

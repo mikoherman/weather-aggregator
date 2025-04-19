906e55630da793fef58405e368c2a437d70cf686
Mikolaj Tworus <mikolaj7933333@gmail.com>
2025-04-20 00:34:45 +0200

    add CountryWeatherDto for easier csv writing

---
+++ WeatherDataAggregator/DTOs/CountryWeatherDto.cs
@@ -0,0 +1,12 @@
+﻿using WeatherDataAggregator.Models;
+
+namespace WeatherDataAggregator.DTOs;
+
+/// <summary>
+/// A data transfer object (DTO) representing weather data for a specific country.
+/// </summary>
+/// <remarks>
+/// This DTO is designed to simplify the process of exporting weather data to CSV files.
+/// It provides a flat structure that is easy to serialize and write to a file.
+/// </remarks>
+public record CountryWeatherDto(string CountryName, string? WeatherDescription, double Temperature, double FeelsLikeTemperature, double Humidity, WindSpeed WindSpeed, double Pressure);

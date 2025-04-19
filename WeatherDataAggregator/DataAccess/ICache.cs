86143d35bdc63420bd8cb9be7c303adc904cb5d2
Mikolaj Tworus <mikolaj7933333@gmail.com>
2025-04-19 21:43:59 +0200

    add ICache interface for abstract representation of caching mechanism for small data sets

---
+++ WeatherDataAggregator/DataAccess/ICache.cs
@@ -0,0 +1,27 @@
+﻿namespace WeatherDataAggregator.DataAccess;
+/// <summary>
+/// Represents a simple cache for storing and managing small data sets.
+/// </summary>
+/// <typeparam name="T">The type of elements to be cached.</typeparam>
+/// <remarks>
+/// This interface is designed for lightweight caching scenarios where the data set is small
+/// </remarks>
+public interface ICache<T>
+{
+    /// <summary>
+    /// Adds the specified elements to the cache.
+    /// </summary>
+    /// <param name="elements">The elements to add to the cache.</param>
+    /// <remarks>
+    /// If an element already exists in the cache, its behavior (e.g., overwrite or ignore) depends on the implementation.
+    /// </remarks>
+    public void AddToCache(params T[] elements);
+}
+
+
+
+
+
+
+
+

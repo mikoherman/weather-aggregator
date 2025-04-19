d2fc653b584aad270f0ce6b189e710d00aafd4ba
Mikolaj Tworus <mikolaj7933333@gmail.com>
2025-04-20 00:55:27 +0200

    add high-level class WeatherDataAggregatorApp

---
+++ WeatherDataAggregator/WeatherDataAggregatorApp.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.ObjectModel;
+using WeatherDataAggregator.DataAccess;
+using WeatherDataAggregator.Models;
+using WeatherDataAggregator.UserInteraction;
+
+namespace WeatherDataAggregator;
+
+public class WeatherDataAggregatorApp
+{
+    private readonly IWeatherDataProvider _weatherDataProvider;
+    private readonly ICountryDataProvider _countryDataProvider;
+    private readonly IConsoleUserIOProcessor _userIOProcessor;
+
+    public WeatherDataAggregatorApp(IWeatherDataProvider weatherDataProvider, 
+        ICountryDataProvider countryDataProvider, IConsoleUserIOProcessor consoleUserIOProcessor)
+    {
+        _weatherDataProvider = weatherDataProvider;
+        _countryDataProvider = countryDataProvider;
+        _userIOProcessor = consoleUserIOProcessor;
+    }
+
+    public async Task Run(string? apiKey)
+    {
+        Continent continent = _userIOProcessor.PromptUserForContinent();
+        ReadOnlyCollection<Country> countryCollection = 
+            await _countryDataProvider.GetCountriesByContinentAsync(continent);
+        ReadOnlyDictionary<Country, WeatherData> countriesWithWeathers = 
+            await _weatherDataProvider
+            .GetWeatherDataForCountriesAsync(countryCollection.Take(2), apiKey);
+        _userIOProcessor.DisplayWeatherDataForCountries(countriesWithWeathers);
+    }
+}

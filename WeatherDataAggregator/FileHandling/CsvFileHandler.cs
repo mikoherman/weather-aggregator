15e5c49e87ff944226ace66a76aec3ebb7db6380
Mikolaj Tworus <mikolaj7933333@gmail.com>
2025-04-20 00:35:24 +0200

    add CsvFileHandler for csv file handling

---
+++ WeatherDataAggregator/FileHandling/CsvFileHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+using CsvHelper;
+
+namespace WeatherDataAggregator.FileHandling;
+
+public class CsvFileHandler<T> : IFileHandler<IEnumerable<T>>
+{
+    public void WriteToAFile(string path, IEnumerable<T> data)
+    {
+        using var writer = new StreamWriter(path);
+        using var csvWriter = new CsvWriter(writer, CultureInfo.InvariantCulture);
+        csvWriter.WriteRecords(data);
+    }
+
+    public IEnumerable<T>? ReadFromAFile(string path)
+    {
+        if (!File.Exists(path))
+            return default(IEnumerable<T>);
+
+        using var reader = new StreamReader(path);
+        using var csvReader = new CsvReader(reader, CultureInfo.InvariantCulture);
+        return csvReader.GetRecords<T>();
+    }
+}
